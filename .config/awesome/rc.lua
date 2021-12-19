d8f56cdaa175dea9690b2a3da375c1708adbf0b7
Glucthu <glucthu@protonmail.com>
2021-12-19 20:06:45 -0300

    added autostart script

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -565,4 +565,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 
 -- Gaps
-beautiful.useless_gap = 5 
+beautiful.useless_gap = 5
+
+-- Autostart
+awful.spawn.with_shell( "~/.config/awesome/autostart.sh")
